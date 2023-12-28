15bf12ac0a25791f6a671bcfc7254c3030bd2d78
Prajyot Tayde <prajyottayde@Prajyots-MacBook-Air.local>
2023-12-29 00:04:48 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        nilla: '#00BAF2',
+        darknilla: '#0F4A89',
+        hover1: '#01AEE3',
+        hover2: '#0C3E80'
+      }
+    },
+  },
+  plugins: [],
+}
+
